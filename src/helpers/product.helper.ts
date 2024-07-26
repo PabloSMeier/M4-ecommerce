f37d3762f698662a9b1746809ce14fb9b4ecb054
psmeier <psmeier05@gmail.com>
2024-07-26 00:23:20 -0300

    .

--- src/helpers/product.helper.ts
+++ src/helpers/product.helper.ts
@@ -13,6 +13,7 @@ export async function getProductsDB() {
       },
       next: { revalidate: 3600 },
     });
+    if (!res.ok) throw new Error("Failed ti fetch data");
     const products: IProduct[] = await res.json();
     return products;
   } catch (error: any) {
